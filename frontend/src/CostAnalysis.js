94c850a11dbf452218cfe4ffd3033c5183048a09
Jon Gunnar <jongunnar0210@gmail.com>
2025-04-07 00:47:24 +0000

    Cost analysis done. Basic requirements done for the whole assignment. Time for cleanup, improvements, testing and prettifying

---
+++ frontend/src/CostAnalysis.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Subscription from './Subscription';
+
+const CostAnalysis = ({costAnalysis}) => {
+    return (
+        <div className="summary-container">
+            <h4>Cost Analysis</h4>
+            {costAnalysis && (
+                <div className="summary-cards">
+                    <div className="card">Total Cost: ${costAnalysis.total_cost.toFixed(0)}</div>
+                    <div className="card">Streaming Cost: ${costAnalysis.streaming_cost.toFixed(0)}</div>
+                    <div className="card">Utilities Cost: ${costAnalysis.utilities_cost.toFixed(0)}</div>
+                    <div className="card">Fitness Cost: ${costAnalysis.fitness_cost.toFixed(0)}</div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default CostAnalysis;
